d43211fe5f10cd5f03d2fcb9d0a8817ca3b08d05
Andrey Lukyanenko <svatoy.a@yandex.ru>
2025-06-14 13:14:58 +0300

    Form on model.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,17 +1,13 @@
 from django import forms 
 
+from .models import Birthdey
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(
-        max_length=20,
-        label='Имя'
-    )
-    last_name = forms.CharField(
-        required=False,
-        label='Фамилия',
-        help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+
+class BirthdayForm(forms.ModelForm):
+
+    class Meta:
+        model = Birthdey
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+class Birthdey(models.Model):
+    first_name = models.CharField(
+        'Имя',
+        max_length=20
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        blank=True,
+        help_text='Необязательное поле',
+        max_length=20
+    )
+    birthdey = models.DateField('Дата рождения')
